0d145a884a0d99746b05fec4965fe8d33bf7f387
김정민 <kim24@gimjeongmins-MacBook-Air.local>
2024-02-26 18:03:10 +0900

    MenuList Test

--- project3/front/src/components/MainItem.vue
+++ project3/front/src/components/MainItem.vue
@@ -2,12 +2,17 @@
   function 로그인가자() {
     location.href = "/login";
   }
+
+  function 메뉴가자() {
+    location.href = "/menuList";
+  }
 </script>
 
 <template>
   <v-main>
     <h2>메인입니다</h2><br>
     <v-btn @click="로그인가자">로그인 테스트 하러가기</v-btn>
+    <v-btn @click="메뉴가자">메뉴 보러가기</v-btn>
   </v-main>
 </template>
 
