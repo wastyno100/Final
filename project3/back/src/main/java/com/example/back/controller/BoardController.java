066140de3bb04a81c9795065e45d7a914841867e
rhfl1757 <rhfl1757@gmail.com>
2024-03-20 18:04:39 +0900

    이벤트, 공지사항 이미지 업로드 및 출력 구현

--- project3/back/src/main/java/com/example/back/controller/BoardController.java
+++ project3/back/src/main/java/com/example/back/controller/BoardController.java
@@ -2,16 +2,39 @@ package com.example.back.controller;
 
 import com.example.back.dto.BoardDto;
 import com.example.back.service.BoardService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class BoardController {
 
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @Autowired
     private BoardService boardService;
+
+    @GetMapping("/getImage/{imgName}")
+    public String getImage(@PathVariable String imgName) throws Exception {
+       String imagePath = "http://localhost:8080/boardImg/" + imgName;
+        System.out.println(imagePath);
+            return imagePath;
+    }
 
     @GetMapping("/boardList")
     @CrossOrigin(origins = "*")
@@ -29,11 +52,45 @@ public class BoardController {
         return boardDetail;
     }
 
+//    @PostMapping("/boardWrite")
+//    @CrossOrigin(origins = "*")
+//    public void boardWrite(@RequestBody BoardDto boardDto) throws Exception {
+//        System.out.println("작성데이터: " + boardDto);
+//        boardService.boardWrite(boardDto);
+//
+//    }
+
     @PostMapping("/boardWrite")
     @CrossOrigin(origins = "*")
-    public void boardWrite(@RequestBody BoardDto boardDto) throws Exception {
+    public ResponseEntity<String> uploadFile(@ModelAttribute BoardDto boardDto,
+                                             @RequestParam(value = "uploadImg", required = false) List<MultipartFile> uploadImgs) throws Exception {
+        System.out.println(uploadImgs);
+        System.out.println(boardDto);
+        List <String> files = new ArrayList<>();
+        Path uploadDir = Paths.get(uploadPath + "/boardImg/");
+
+        for (MultipartFile boardImg : uploadImgs) {
+            // 이미지 uuid 설정
+            String uuid = UUID.randomUUID().toString();
+            String fileName = StringUtils.cleanPath(uuid + "_" + boardImg.getOriginalFilename());
+
+            // UUID 설정 된 파일 이름을 dto에 넣고 db에 저장하자
+            files.add(fileName);
+
+            // 이미지를 업로드할 경로 설정
+            Path filePath = uploadDir.resolve(fileName);
+
+            // 이미지를 저장
+            Files.copy(boardImg.getInputStream(), filePath);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String filesJson = objectMapper.writeValueAsString(files);
+
+        boardDto.setBoardImg(filesJson);
+
         boardService.boardWrite(boardDto);
 
+        return ResponseEntity.ok("저장완료");
     }
 
     @PutMapping("/boardUpdate")
--- project3/front/src/components/board/BoardEvent.vue
+++ project3/front/src/components/board/BoardEvent.vue
@@ -1,17 +1,23 @@
 <script setup>
 import axios from 'axios'
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-const testData = ref([])
+const boardData = ref([])
+
+
+// 현재 날짜
+const date = new Date()
+const today = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`
 
 // 페이징용
 const currentPage = ref(1)
-const pageGroup = 10
+const pageGroup = 5
 const allPage = computed(() => {
-  return Math.ceil(testData.value.length / pageGroup)
+  return Math.ceil(boardData.value.length / pageGroup)
 }) // 데이터의 개수를 페이지당 보여줄 항목수로 나누고 올림 함
+
 
 // 검색용
 const searchText = ref("")
@@ -20,41 +26,68 @@ const searchCate = ref("제목")
 const getData = async () => {
   const res = await axios.get('/api/boardList?boardCate=이벤트')
   
-  // 데이터 내림차순 (공지, 이벤트 만들때 써보자)
-  testData.value = res.data.sort((a, b) => b.boardNo - a.boardNo)
+  // 데이터 내림차순
+  boardData.value = res.data.sort((a, b) => b.boardNo - a.boardNo)
 
-  testData.value.forEach((item) => {
+  boardData.value.forEach(async (item) => {
     item.boardDate = item.boardDate.replace(/^(\d{4})-(\d{2})-(\d{2}).*/, '$1-$2-$3')
+
+    // 1. 받은 데이터의 이미지 이름 parse
+    // 2. 이미지 이름으로 요청
+    if(item.boardImg != null) {
+      item.boardImg = JSON.parse(item.boardImg)
+      await axios.get(`/api/getImage/${item.boardImg[0]}`)
+      .then((res) => {
+      item.boardImg = res.data
+    })
+    }
+
+
   })
 }
-
-onMounted(() => {
-  getData()
-})
 
 const showItem = computed(() => {
   const start = (currentPage.value - 1) * pageGroup
   const end = start + pageGroup
   if(searchFind.value) return searchFind.value
-  else return testData.value.slice(start, end)
+  else return boardData.value.slice(start, end)
 })
 
 // 페이지가 바뀔때 해당하는 항목으로 업데이트
 const pageUpdate = () => {
   const start = (currentPage.value - 1) * pageGroup
   const end = start + pageGroup
-  showItem.value = testData.value.slice(start, end)
+  showItem.value = boardData.value.slice(start, end)
 }
 
 // 검색 텍스트에 맞는 항목을 필터링
 const searchFind = computed(() => {
   if(searchText.value) {
-    return testData.value.filter((item) => {
+    return boardData.value.filter((item) => {
       if(searchCate.value == "제목") return item.title.includes(searchText.value) 
       if(searchCate.value == "내용") return item.content.includes(searchText.value)
     })
   }
   else return ""
+})
+
+const goDetail = (item) => {
+  router.push({
+    name: 'detail',
+    state: {dataObj: { no: item.boardNo }}
+  })
+}
+
+// 현재 페이지 감지해서 세션에 저장
+watch(currentPage, () => {
+  sessionStorage.setItem('eventPage', currentPage.value)
+})
+
+onMounted(() => {
+  getData()
+
+  // 페이지 돌아왔을때 세션에 저장되어있는 페이지 가져옴
+  currentPage.value = JSON.parse(sessionStorage.getItem('eventPage')) || 1
 })
 </script> 
   
@@ -92,17 +125,17 @@ const searchFind = computed(() => {
         v-bind:key="item"
         class="text-center mt-1"
         :class="{ 'v-row-hover': true }"
-        @click="
-          router.push({
-            name: 'detail',
-            state: {dataObj: { no: item.boardNo }}
-          })
-        ">
-        <v-row>
+        @click="goDetail(item)">
+        <v-row align="center">
           <v-col>
             <span>{{ item.boardNo }}</span>
           </v-col>
           <v-col>
+            <img :src="item.boardImg" :width="60" />
+          </v-col>
+          <v-col>
+            <span v-if="item.eventEnd > today" style="color: blue;">(진행중) </span>
+            <span v-if="item.eventEnd < today" style="color: red;">(종료) </span>
             <span>{{ item.title }}</span>
           </v-col>
           <v-col>
